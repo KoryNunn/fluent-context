4bd2654e179279196924c35854cd4868ccaf8ff6
Kory Nunn <knunn187@gmail.com>
2019-02-05 15:58:55 +1000

    Failing functional api test

--- test/index.js
+++ test/index.js
@@ -11,6 +11,18 @@ var createApi = (fooValue = 0) => ({
     }
 });
 
+var createFunctionBasedApi = (fooValue = 0) => {
+    var fnApi = function(){
+        return fnApi._foo;
+    }
+    fnApi._foo = fooValue;
+    fnApi.bar = function(){
+        return createFunctionBasedApi(this._foo + 1);
+    };
+
+    return fnApi;
+};
+
 test('test api works', t => {
     t.plan(3);
 
@@ -19,7 +31,21 @@ test('test api works', t => {
     t.equal(api.foo(), 0);
     t.equal(api.bar().foo(), 1);
     t.equal(api.bar().bar().foo(), 2);
-})
+});
+
+test('test functionApi works', t => {
+    t.plan(4);
+
+    var api = createFunctionBasedApi();
+
+    t.equal(api(), 0);
+    t.equal(api.bar()(), 1);
+    t.equal(api.bar().bar()(), 2);
+
+    var bar = api.bar;
+
+    t.notEqual(bar(), 1);
+});
 
 test('single level', t => {
     t.plan(1);
@@ -50,4 +76,19 @@ test('multiple levels', t => {
     var barBarFoo = bar().bar().foo;
 
     t.equal(barBarFoo(), 2);
+});
+
+test('works for function-apis', t => {
+    t.plan(2);
+
+    var api = createFunctionBasedApi();
+    var wrappedApi = fluentContext(api);
+
+    var bar = wrappedApi.bar;
+
+    t.equal(bar()(), 0);
+
+    var barBar = bar().bar();
+
+    t.equal(barBar(), 1);
 });
